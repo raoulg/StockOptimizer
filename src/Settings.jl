e2e9850fd9b55b273cecde834d5393ccd1687b40
Raoul Grouls <raoul.grouls@businessdecision.nl>
2022-10-06 10:22:51 +0200

    Settings

---
+++ src/Settings.jl
@@ -0,0 +1,32 @@
+module Settings
+
+export Config
+import Base.@kwdef
+using ..Pathlib
+
+@kwdef struct Config
+    datadir::Path 
+    processeddir::Path
+    suffix::String
+    staticfiles::Vector{Pathlib.Path}
+    dynamicfiles::Vector{Pathlib.Path}
+    trainfile::Pathlib.Path
+    dynamic_map::Dict{
+        String,
+        NamedTuple{(:datecol, :valcol),Tuple{String,Vector{String}}}}
+    col_map::Dict{String, Symbol}
+    minlen::Int
+end
+
+@kwdef struct PreprocessConfig
+    datecol::Symbol
+    none_cols::Vector{Symbol}
+    locf_cols::Vector{Symbol}
+    zero_cols::Vector{Symbol}
+    drop_cols::Vector{Symbol}
+    group_cols::Vector{Symbol}
+    stepsize::Int
+    smoketest::Int
+end
+
+end
