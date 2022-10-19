a60b171457a3ac6a7a3dba4f36b10c1439302398
Raoul Grouls <raoul.grouls@businessdecision.nl>
2022-10-19 14:01:35 +0200

    integrate Observation struct in Data.jl

--- src/Data.jl
+++ src/Data.jl
@@ -1,84 +1,117 @@
 module Data
 
 using Random: shuffle
-using DataFrames: DataFrame
-export TimeSeriesItem, BufferedLoader
+using DataFrames: DataFrame, select, combine, GroupedDataFrame, AbstractDataFrame
+using Base: @kwdef
+export TimeSeriesItem, BufferedLoader, sizes
 
 
-struct TimeSeriesItem{D, T}
-    data::D
-    keys::T
+struct TimeSeriesItem{
+  D1<:GroupedDataFrame,
+  D2<:GroupedDataFrame,
+  K<:Vector{Tuple{Int,Int}},
+}
+  dynamic::D1
+  static::D2
+  keys::K
 end
 
 # constructor
-TimeSeriesItem(data) = TimeSeriesItem(data, shuffle(collect(keys(data))))
+TimeSeriesItem(data, config) = TimeSeriesItem(
+  select(data, vcat(config.dyn_cat, config.dyn_real), ungroup = false),
+  combine(
+    data,
+    vcat(config.stat_cat, config.stat_real) .=> unique,
+    ungroup = false,
+    renamecols = false,
+  ),
+  shuffle(Tuple.(keys(data))),
+)
+
+@kwdef struct Observation{D<:AbstractDataFrame,S<:AbstractDataFrame}
+  dynamic::D
+  static::S
+end
 
 # getobs and numobs, see https://github.com/JuliaML/MLUtils.jl for details
-getobs(ts::TimeSeriesItem, idx) = ts.data[ts.keys[idx]]
+getobs(ts::TimeSeriesItem, idx) =
+  Observation(ts.dynamic[ts.keys[idx]], ts.static[ts.keys[idx]])
 numobs(ts::TimeSeriesItem) = length(ts.keys)
 
 # split dataframe into stepsized chunks
-function split_ts(ts, stepsize)
-    startidx = collect(1:stepsize:size(ts)[1])[1:end-1]
-    idx = [i:i+stepsize-1 for i in startidx]
-    [ts[i, :] for i in idx]
+function split_df(ts::AbstractDataFrame, stepsize::Int)
+  startidx = collect(1:stepsize:size(ts)[1])[1:end-1]
+  idx = [i:i+stepsize-1 for i in startidx]
+  [ts[i, :] for i in idx]
 end
 
 #iterator
-Base.iterate(ts::TimeSeriesItem, state=1) = state > length(ts.keys) ? nothing : (getobs(ts, state), state+1)
+Base.iterate(ts::TimeSeriesItem, state = 1) =
+  state > length(ts.keys) ? nothing : (getobs(ts, state), state + 1)
 
 # Buffer
 mutable struct BufferedLoader
-    ts::TimeSeriesItem
-    batchsize::Int
-    buffer::Vector{DataFrame}
-    count::Int
+  ts::TimeSeriesItem
+  batchsize::Int
+  buffer::Vector{Observation}
+  count::Int
+  stepsize::Int
 end
 
 # constructor
-BufferedLoader(ts::TimeSeriesItem, batchsize::Int) = BufferedLoader(ts, batchsize, [], length(ts.keys))
-
-function split_ts(ts, stepsize)
-    startidx = collect(1:stepsize:size(ts)[1])[1:end-1]
-    idx = [i:i+stepsize-1 for i in startidx]
-    [ts[i, :] for i in idx]
-end
+BufferedLoader(ts::TimeSeriesItem, batchsize::Int, stepsize::Int) =
+  BufferedLoader(ts, batchsize, [], length(ts.keys), stepsize)
 
 """
 This empties the buffer
 """
-function (b::BufferedLoader)()
-    # dont run if everything is consumed
-    if b.count == 0
-        return nothing
-    end
+function (b::BufferedLoader)()::Union{Vector{Observation},Nothing}
+  # dont run if everything is consumed
+  if b.count == 0
+    return nothing
+  end
 
-    # if the buffer is too small
-    if length(b.buffer) < b.batchsize
-        # while we can consume items
-        while (b.count > 0) & (length(b.buffer) < b.batchsize)
-            # get and observation
-            ts = getobs(b.ts, b.count)
-            # split into stepsizes
-            for tsx in split_ts(ts, 120)
-                push!(b.buffer, tsx)
-            end
-            # consume the count
-            b.count = b.count - 1
-        end
+  # if the buffer is too small
+  if length(b.buffer) < b.batchsize
+    # while we can consume items
+    while (b.count > 0) & (length(b.buffer) < b.batchsize)
+      # get and observation
+      obs = getobs(b.ts, b.count)
+      # split into stepsizes
+      for tsx in split_df(obs.dynamic, b.stepsize)
+        push!(b.buffer, Observation(tsx, obs.static))
+      end
+      # consume the count
+      b.count = b.count - 1
     end
+  end
 
-    # if we need to reduce the buffer
-    if length(b.buffer) >= b.batchsize
-        out = b.buffer[1:b.batchsize]
-        b.buffer = b.buffer[b.batchsize:end]
-    else
-        out = b.buffer
-    end
+  # if we need to reduce the buffer
+  if length(b.buffer) >= b.batchsize
+    out = b.buffer[1:b.batchsize]
+    b.buffer = b.buffer[b.batchsize:end]
+  else
+    return nothing
+    # out = b.buffer
+  end
 
-    return out
+  return out
 end
 
-Base.iterate(b::BufferedLoader, state=1) = b.count <= 0 ? nothing : (b(), state+1)
+# Base.iterate(b::BufferedLoader, state=1) = b.count <= 0 ? nothing : (b(), state+1)
+Base.iterate(b::BufferedLoader, state = 1) = b.count <= 0 ? nothing : (b(), state + 1)
+
+function sizes(x, prefix = ">")
+  if eltype(x) <: AbstractArray
+    container = first(split(string(typeof(x)), "{"))
+    println(prefix, container * ": " * string(size(x)))
+    for xi in x
+      sizes(xi, prefix * prefix)
+    end
+  else
+    container = first(split(string(typeof(x)), "{"))
+    println(prefix, container * ": " * string(size(x)))
+  end
+end
 
 end
