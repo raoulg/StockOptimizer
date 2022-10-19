1af2fdeee3a50db804d2575983775b947f92db5b
Raoul Grouls <raoul.grouls@businessdecision.nl>
2022-10-19 14:00:24 +0200

    linting

--- src/Paths.jl
+++ src/Paths.jl
@@ -14,24 +14,24 @@ getsuffix(p::String) = occursin(".", p) ? last(split(p, ".")) : ""
 
 #
 struct Path
-    path::String
-    name::String
-    suffix::String
-    Path(p::String) = new(p, getname(p), getsuffix(p))
+  path::String
+  name::String
+  suffix::String
+  Path(p::String) = new(p, getname(p), getsuffix(p))
 end
 
-Base.:/(a::Path, b::Path) = Path(a.path*"/"*b.path)
-Base.:/(a::Path, b::String) = Path(a.path*"/"*b)
+Base.:/(a::Path, b::Path) = Path(a.path * "/" * b.path)
+Base.:/(a::Path, b::String) = Path(a.path * "/" * b)
 Base.:(==)(a::Path, b::String) = a.path == b
 Base.show(io::IO, p::Path) = print(io, p.path)
 
 
 function hassuffix(f::String, suffix::String)
-    return split(f, ".")[2] == suffix
+  return split(f, ".")[2] == suffix
 end
 
 function hassuffix(f::Path, suffix::String)
-    return f.suffix == suffix
+  return f.suffix == suffix
 end
 
 end
