bf768da796e4e4862f1e5c96d8f161369b11094c
Raoul Grouls <raoul.grouls@businessdecision.nl>
2022-10-19 14:02:01 +0200

    add Abstract3DArray to Layers.jl

--- src/Layers.jl
+++ src/Layers.jl
@@ -1,24 +1,29 @@
 module Layers
 
+using Flux: @functor, glorot_uniform
 function Hadamard(x1, x2)
-    x1.*x2
+  x1 .* x2
 end
 
 function splitmatrix(m)
-    return [m[i, :] for i in 1:size(m, 1)]
+  return [m[i, :] for i = 1:size(m, 1)]
 end
 
 
 struct Split{T}
-    paths::T
+  paths::T
 end
-  
+
 Split(paths...) = Split(paths)
-  
-using Flux: @functor
+
 @functor Split
-  
+
 (m::Split)(x::AbstractArray) = map(f -> f(x), m.paths)
 
+# type for 3D data
+const Abstract3DArray{T} = AbstractArray{T, 3}
+
+# fast fill of a bias, with size of the first dimension of the weights
+create_bias(weights::AbstractArray) = fill!(similar(weights, size(weights, 1)), 0)
 
 end
