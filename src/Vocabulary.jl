ae5b942912c3212d0626fa07c13859cc4d1d1c78
Raoul Grouls <raoul.grouls@businessdecision.nl>
2022-10-19 13:59:11 +0200

    restructured Vocabulary

--- src/Vocabulary.jl
+++ src/Vocabulary.jl
@@ -4,40 +4,50 @@ using DataStructures
 using ProgressBars
 using DataFrames
 using FileIO
+using Dates
 
 using ..Settings
 
-mutable struct Vocab
-    data::DefaultDict{String,Int,Int}
-    len::Int
-    Vocab(x::DefaultDict{String,Int,Int}) = new(x, length(x))
+mutable struct Vocab{D<:Union{DefaultDict{String, Int, Int}, DefaultDict{Int, Int, Int}}}
+  data::D
+  len::Int
+  Vocab(d::DefaultDict) = new{typeof(d)}(d, length(d))
 end
 
-Base.getindex(v::Vocab, key::String) = v.data[key]
+Base.getindex(v::Vocab, key) = v.data[key]
 
-function build_vocab(corpus::Vector{String})::Vocab
-    d = DefaultDict{String,Int}(0)
-    d["<OOV>"] = 0
-    for key in unique(corpus)
-        d[string(key)] = length(d)
-    end
-    return Vocab(d)
+function build_vocab(corpus::Vector{T}) where T<:Union{String, Int}
+  d = DefaultDict{T, Int, Int}(1)
+  if T == String
+      d["<OOV>"] = 1
+  else
+      d[typemax(T)] = 1
+  end
+
+  for key in unique(corpus)
+      d[key] = length(d) + 1
+  end
+  return Vocab(d)
 end
 
-isstringcol(x) = eltype(x) <: String
+function Base.show(io::IO, v::Vocab{DefaultDict{T, Int, Int}}) where {T}
+  println(io, "Vocab($T=>Int, default=$(v.data.d.default))")
+end
 
-function extract_vocabs(df::DataFrame, config::Settings.Config)::Dict{String,Vocab}
-    stringcols = findall(isstringcol, eachcol(df))
-    vocabs = Dict{String,Vocab}()
-    for col in ProgressBar(names(df[!, stringcols]))
-        vals = unique(df[!, col])
-        v = build_vocab(vals)
-        vocabs[String(col)] = v
-    end
-    vocabpath = config.processeddir / "vocabs.jld2"
-    @info "Saving vocabs to $(vocabpath.path)"
-    save(vocabpath.path, Dict("vocabs" => vocabs))
-    return vocabs
+
+function extract_vocabs(df::DataFrame, dataconfig::Settings.DataConfig, dir)::Dict{Symbol,Vocab}
+  cols = vcat(dataconfig.dyn_cat , dataconfig.stat_cat)
+  vocabs = Dict{Symbol,Vocab}()
+  for col in ProgressBar(names(df[!, cols]))
+    vals = collect(skipmissing(unique(df[!, col])))
+    v = build_vocab(vals)
+    vocabs[Symbol(col)] = v
+  end
+  tag = Dates.format(now(), "yyyymmdd-HHMMSS")
+  vocabpath = dir / (tag * "-vocabs.jld2")
+  @info "Saving vocabs to $(vocabpath.path)"
+  save(vocabpath.path, Dict("vocabs" => vocabs))
+  return vocabs
 end
 
 end
